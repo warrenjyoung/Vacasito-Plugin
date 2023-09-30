a99d4e22b2e463078343e932f1c20e7e0365351e
Warren <warrenjyoung@gmail.com>
2023-09-30 15:35:32 +0000

    updated plugin name

--- vacasito/vacasito.php
+++ vacasito/vacasito.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              https://vacasito.com/
  * @since             1.0.0
  * @package           Vacasito
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/vacasito-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Vacasito Plugin
+ * Plugin URI:        https://vacasito.com/
+ * Description:       This plugin allows vacasito to manage content.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Vacasito
+ * Author URI:        https://vacasito.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       vacasito
